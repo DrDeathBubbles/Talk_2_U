7940154b4f551e03a5a7fa5425d69a36e1b6e4e0
DrDeathBubbles <Aaron.Meagher@gmail.com>
2021-03-03 12:40:36 +0000

    More changes

--- hub_2.py
+++ hub_2.py
@@ -4,18 +4,28 @@ from talkbot_redis import redis_control_database
 from aws import get_object_url
 
 
-def __main__(region, input_queue):
-    r = redis_control_database(6379)
-    s3_queue = sqs_queue('DS_AJM_VIDEO')
+def __main__(region,redis_port, input_queue):
+    r = redis_control_database(redis_port)
+    s3_queue = sqs_queue(input_queue)
 
     talkbot_priority = sqs_queue('talkbot_priority')
     talkbot_normal = sqs_queue('talkbot_normal')
+    talkbot_transcription = sqs_queue('talkbot_transcription')
 
     
     while True:
         messsages = s3_queue.get_sqs_message()
         for message in messages:
             key = data_format_s3(message)
+            if r.check_exists:
+                #Processing to priority or normal
+                #Controlled flow
+
+                r.get_field('')
+                pass
+            else:
+                #Here do everything
+                r.make_record(key)
             url = get_object_url(key)
             r.make_record(key)
             r.update_field(key,'s3_raw',url)
