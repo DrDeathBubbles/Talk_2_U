c786abf4530b4af87c6e892fc4e904d541587f3c
DrDeathBubbles <aaron.meagher@gmail.com>
2021-03-03 15:09:24 +0000

    Renaming to lower case file name

--- Talkbot_sqs.py
+++ Talkbot_sqs.py
@@ -8,13 +8,13 @@ class sqs_queue:
         self.sqs = boto3.resource('sqs', region_name = 'eu-west-1')
         self.queue = self.sqs.get_queue_by_name(QueueName=queue_name)
 
-    def get_sqs_message():
+    def get_sqs_message(self,):
         out = []
         messages = self.queue.receive_messages()
         for m in messages:
-            out.append(json.loads(m.body()))
+            out.append(json.loads(m.body)
             m.delete()
         return out    
     
-    def send_sqs_message(message):
+    def send_sqs_message(self, message):
         response = self.queue.send_message(MessageBody=message)
