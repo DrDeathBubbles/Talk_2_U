764491f471d155dc4f5715b35f5ac9c1c07a8e74
DrDeathBubbles <aaron.meagher@gmail.com>
2021-03-28 21:12:08 +0100

    Making changes to two

--- transcription_main.py
+++ transcription_main.py
@@ -1,5 +1,29 @@
-from transcription.transcription import transcribe
+from tools.talkbot_redis import redis_control_database
+from transcription.transcription import transcribe 
 
 
-def main():
+def main(redis_port = '6379', transcription_queue_name = 'talkbot_transcription',
+local_file_location = '/'):
+    transcription_queue = sqs.queue(transcription_queue_name)
+    r = redis_control_database(redis_port)
+    t = transcribe()
+    while True:
+        messages = transcription_queue.get_sqs_message() 
+        for message in messages:
+           t. 
+
+
+
+
+
+
+            local_file = local_file_location + message
+            data = r.get_talk_data(message)
+            bucket.retrieve_from_s3(message, local_file)
+            if data :
+                vimeo_url = vimeo_upload(local_file, d['title'],d['description'])
+            else:
+                vimeo_url = vimeo_upload(local_file,'Unset','Unset','private')     
+    
+            r.update_field(self, message,'vimeo',vimeo_url)
     
