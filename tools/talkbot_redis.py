463c5ee42a996c568a796fc2e80eef9d79a174b6
DrDeathBubbles <aaron.meagher@gmail.com>
2021-03-30 20:06:57 +0100

    Changes to redis

--- tools/talkbot_redis.py
+++ tools/talkbot_redis.py
@@ -17,7 +17,7 @@ class redis_control_database:
         self.port = port
         self.conn = redis.StrictRedis(host='localhost', port = port, db=0, decode_responses=True)
 
-    def check_exits_redis(self, key):
+    def check_exsits_redis(self, key):
         if self.conn.exists(key):
             return True
         else:
@@ -39,20 +39,26 @@ class redis_control_database:
         if self.check_exits_redis(key) and self.check_field_in_schema_fields(field):
             self.conn.hmset(key,field,value)
         else:
-            print('Did not set field')
+            #logging.error(f'{key},{field} not available')
+            print(f'{key},{field} not available')
 
 
     def get_field(self, key, field):
         if self.check_exits_redis(key) and self.check_field_in_schema_fields(field):
             data = self.conn.hget(key,field)
         else:
-            print('Did not set field')
+            #logging.error(f'{key},{field} not available')
+            print(f'{key},{field} not available')
             data = {}
         return data
 
     def get_data(self, key):
-        assert self.check_exists_redis(key), 'Field not in schema'
-        data = self.conn.hgetall(key)
+        if self.check_exsits_redis(key): 'Field not in schema'
+            data = self.conn.hgetall(key)
+        else:
+            #logging.error(f'{key} does not exist in redis')
+            print(f'{key} does not exist in redis')    
+            data = {}
         return data
 
 
