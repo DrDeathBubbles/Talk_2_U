52f24056410c24680f9f8d92c822393602380ef2
DrDeathBubbles <aaron.meagher@gmail.com>
2021-04-01 16:56:43 +0100

    Using absolute paths

--- testing_code/testing_code.py
+++ testing_code/testing_code.py
@@ -1,10 +1,11 @@
 from tools.talkbot_s3 import s3_bucket
+import os
 
 
 
 def main():
     input_bucket = s3_bucket('cc21-raw')
-    input_bucket.post_to_s3('./test/test_video.mp4','test_video.mp4')
+    input_bucket.post_to_s3(os.path.abspath('./test/test_video.mp4'),'test_video.mp4')
 
 
 if __name__ == '__main__':
