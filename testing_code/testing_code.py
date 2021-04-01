718173cdf0afa16a1e82962de89cb1382b41853a
DrDeathBubbles <aaron.meagher@gmail.com>
2021-04-01 16:58:43 +0100

    Change to path

--- testing_code/testing_code.py
+++ testing_code/testing_code.py
@@ -5,7 +5,7 @@ import os
 
 def main():
     input_bucket = s3_bucket('cc21-raw')
-    input_bucket.post_to_s3(os.path.abspath('./test/test_video.mp4'),'test_video.mp4')
+    input_bucket.post_to_s3(os.path.abspath('./testing_code/test_video.mp4'), 'test_video.mp4')
 
 
 if __name__ == '__main__':
